c87c854e16adff8dd77587f3a59ce581c8f94f4b
lilyshin <lilyshin.dev@gmail.com>
2022-12-22 03:46:30 +0900

    CRUD 추가

--- src/main/java/com/project/tour/configuration/WebMvcConfig.java
+++
@@ -1,8 +0,0 @@
-package com.project.tour.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-}
